55cb8ed6775cb0282cd623ed9471c17a2423e9a7
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-08-10 21:11:32 +0900

    작성 페이지 기본 기능 구현완료, 이미지 미리보기 삭제 구현 중

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -1,33 +1,30 @@
-import React, { useRef } from 'react';
+/* eslint-disable react/no-array-index-key */
+import React, { useRef, useState } from 'react';
 
 // eslint-disable-next-line no-undef
 const New = (): JSX.Element => {
-  //   const [images, setImages] = useState<string[]>([]);
+  const [images, setImages] = useState<string[]>([]);
+  const fileList: File[] = [];
   const title = useRef<HTMLInputElement>(null);
   const peopleDeadline = useRef<HTMLInputElement>(null);
   const matchDeadline = useRef<HTMLInputElement>(null);
   const subject = useRef<HTMLSelectElement>(null);
   const content = useRef<HTMLTextAreaElement>(null);
-
-  //   const fileUploader = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //     const files = e.target.files!;
-  //     if (!files[0]) return;
-  //     if (images.length + files.length > 3) {
-  //       return alert('이미지는 세장까지 업로드 가능합니다.');
-  //     }
-  //     const readAndPreview = (file: File) => {
-  //       const reader = new FileReader();
-  //       reader.onload = () => setImages((prev) => [...prev, reader.result as string]);
-  //       reader.readAsDataURL(file);
-  //     };
-  //     if (files) {
-  //       [].forEach.call(files, readAndPreview);
-  //     }
-  //   };
-
-  const fileList: File[] = [];
-
   const onSaveFiles = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files!;
+    if (!files[0]) return;
+    if (images.length + files.length > 3) {
+      // eslint-disable-next-line consistent-return
+      return alert('이미지는 세장까지 업로드 가능합니다.');
+    }
+    const readAndPreview = (file: File) => {
+      const reader = new FileReader();
+      reader.onload = () => setImages((prev) => [...prev, reader.result as string]);
+      reader.readAsDataURL(file);
+    };
+    if (files) {
+      [].forEach.call(files, readAndPreview);
+    }
     const uploadFiles = Array.prototype.slice.call(e.target.value);
 
     uploadFiles.forEach((uploadFile) => {
@@ -52,16 +49,27 @@ const New = (): JSX.Element => {
 
     // axios 자리
   };
+  const deleteImage = (index: any) => {
+    const imgArr = images.filter((el, id) => id !== index);
+    setImages(imgArr);
+  };
+
+  const previewImage = () => {
+    return images.map((image, index) => (
+      <div key={index}>
+        <img className='w-40 h-40 bg-black' alt='' src={image} />
+        <button type='button' onClick={deleteImage}>
+          삭제
+        </button>
+      </div>
+    ));
+  };
 
   return (
     <section className='flex flex-col justify-center bg-gray-200 w-full h-screen '>
       제목
       <input className='mb-3 w-full' type='text' ref={title} />
-      <section className='flex flex-row h-1/2 justify-center items-center'>
-        {/* <img className='w-1/3 h-full bg-black' alt='' src={images[0]} />
-        <img className='w-1/3 h-full bg-black' alt='' src={images[1]} />
-        <img className='w-1/3 h-full bg-black' alt='' src={images[2]} /> */}
-      </section>
+      <section className='flex h-1/2 justify-center items-center'>{previewImage()}</section>
       <input type='file' multiple onChange={onSaveFiles} />
       <section className='flex flex-row gap-1'>
         <div>
@@ -69,19 +77,19 @@ const New = (): JSX.Element => {
           <input className='w-full' type='text' ref={matchDeadline} />
         </div>
         <div>
-          <div>모집인원</div>
+          <div>경기마감일</div>
           <input className='w-full' type='text' ref={peopleDeadline} />
         </div>
       </section>
       종목
       <select className='w-full text-center' ref={subject}>
-        <option value='subject'>-종목을 골라주세요-</option>
-        <option value='soccer'>축구</option>
-        <option value='basketball'>농구</option>
-        <option value='badminton'>배드민턴</option>
-        <option value='billiards'>당구</option>
-        <option value='bowling'>볼링</option>
-        <option value='tennis'>테니스</option>
+        <option value='SUBJECT'>-종목을 골라주세요-</option>
+        <option value='SOCCER'>축구</option>
+        <option value='BASKETBALL'>농구</option>
+        <option value='BADMINTON'>배드민턴</option>
+        <option value='BILLIARDS'>당구</option>
+        <option value='BOWLING'>볼링</option>
+        <option value='TENNIS'>테니스</option>
       </select>
       내용
       <textarea className='mb-5 w-full h-2/5' ref={content} />
