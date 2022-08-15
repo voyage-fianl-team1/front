640677ff45173f66ef2e08897eb56ec2b6b8bd3e
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-08-15 19:28:25 +0900

    현재 위치 기반 지도 포현, 마커 구현, 오버레이 구현

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -1,16 +1,51 @@
-import React, { useRef, useState } from 'react';
-import { Map, ZoomControl, MapMarker } from 'react-kakao-maps-sdk';
+import React, { useRef, useState, useEffect } from 'react';
+import { Map, ZoomControl, MapMarker, MapInfoWindow } from 'react-kakao-maps-sdk';
 
 const MapContainer = () => {
   const mapRef = useRef(null);
   const [position, setPosition] = useState({ lat: 0, lng: 0 });
-  console.log(position.lat, position.lng);
+  const [state, setState] = useState({
+    center: {
+      lat: 37.6378799247,
+      lng: 127.020584374,
+    },
+    errMsg: 'test',
+    isLoading: true,
+  });
+  // const [address, setAddress] = useState<string>();
+  useEffect(() => {
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition(
+        (positions) => {
+          setState((prev) => ({
+            ...prev,
+            center: {
+              lat: positions.coords.latitude, // 위도
+              lng: positions.coords.longitude, // 경도
+            },
+            isLoading: false,
+          }));
+        },
+        (err) => {
+          setState((prev) => ({
+            ...prev,
+            errMsg: err.message,
+            isLoading: false,
+          }));
+        }
+      );
+    } else {
+      setState((prev) => ({
+        ...prev,
+        errMsg: 'geolocation을 사용할수 없어요..',
+        isLoading: false,
+      }));
+    }
+  }, []);
   return (
     <Map
-      center={{
-        lat: 37.6378799247,
-        lng: 127.020584374,
-      }}
+      center={{ lat: state.center.lat, lng: state.center.lng }}
       className='w-screen h-screen'
       level={3}
       ref={mapRef}
@@ -21,7 +56,10 @@ const MapContainer = () => {
         })
       }
     >
-      {position && <MapMarker position={position} />}
+      <MapInfoWindow position={{ lat: position.lat, lng: position.lng }} removable>
+        <div>Hello World</div>
+      </MapInfoWindow>
+      {position && <MapMarker position={position} draggable />}
       <ZoomControl position={window.kakao.maps.ControlPosition.TOPRRIGHT} />
     </Map>
   );
