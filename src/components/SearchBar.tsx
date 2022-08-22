8e9061d82e3409001280099074d6875cc75c84f2
livemehere <rhdxoals@gmail.com>
2022-08-22 15:01:19 +0900

    feature : 공통 네브바

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 const SearchBar = () => {
+  const [isFocus, setIsFocus] = useState(false);
+  const handleFocus = useCallback(() => {
+    setIsFocus(true);
+  }, []);
+
   return (
     <div className='flex border-[1px] border-matchgi-gray h-[44px] rounded-[12px] items-center p-[5px]'>
       <img src='/assets/images/search.svg' alt='search-icon' className='w-[24px] h-[24px] mx-[10px]' />
-      <input type='text' placeholder='경기, 종목, 키워드 검색' className='flex-1 outline-0' />
+      <input type='text' placeholder='경기, 종목, 키워드 검색' className='flex-1 outline-0' onFocus={handleFocus} />
     </div>
   );
 };
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,11 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useMemo, useState } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 interface Props {
   children: JSX.Element;
 }
 
 const Layout: FC<Props> = ({ children }) => {
-  return <div className='p-3 max-w-[1000px] m-auto'>{children}</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleRouteBack = useCallback(() => {
+    navigate(-1);
+  }, []);
+
+  const navShow = useMemo(() => {
+    const path = location.pathname;
+    return !(/login|signup/g.test(path) || path === '/');
+  }, [location.pathname]);
+
+  return (
+    <div className='p-3 max-w-[1000px] m-auto'>
+      {navShow && (
+        <nav className='flex gap-3 items-center py-4'>
+          <img src='/assets/images/back.svg' alt='back-button' onClick={handleRouteBack} />
+          <h1 className='text-lg font-bold'>현재 페이지이름</h1>
+        </nav>
+      )}
+      <div>{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
