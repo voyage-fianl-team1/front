2e9192afb482b02c15b6003b2008582a634b5d7f
livemehere <rhdxoals@gmail.com>
2022-08-27 02:13:18 +0900

    fix : 채팅 검색 기능 추가 & 커스텀 디바운스 적용

--- src/pages/ChatListPage.tsx
+++ src/pages/ChatListPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import ChatSearchBar from '../components/ChatSearchBar';
 import ChatRoomItem from '../components/ChatRoomItem';
 import { useQuery } from '@tanstack/react-query';
@@ -9,19 +9,36 @@ import { useInput } from '../hooks/useInput';
 import { Helmet } from 'react-helmet';
 
 const ChatListPage = () => {
-  const { data } = useQuery<ChatRoom[]>(['chatRooms'], apis.getChatRooms);
   const { value, handler } = useInput('');
+  const [filterData, setFilterData] = useState<ChatRoom[]>([]);
+  const timerRef = useRef<any>(null);
 
-  const filterdChatRoomList = useMemo(() => {
-    if (!data) return [];
-    return [...data].filter((chat) => chat.title !== value);
+  const { data } = useQuery<ChatRoom[]>(['chatRooms'], apis.getChatRooms, {
+    select: (data) => data.filter((d) => d.title !== value),
+  });
+
+  useEffect(() => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    if (!data) {
+      setFilterData([]);
+    } else {
+      if (value.length === 0) {
+        setFilterData(data);
+      }
+      const filtering = () => {
+        const regExp = new RegExp(value, 'g');
+        console.log('호출', 1);
+        setFilterData(data.filter((d) => regExp.test(d.title)));
+      };
+      timerRef.current = setTimeout(filtering, 500);
+    }
   }, [data, value]);
 
   if (!data) {
     return <LoadingSpinner />;
   }
-
-  console.log(filterdChatRoomList);
 
   return (
     <>
@@ -31,7 +48,7 @@ const ChatListPage = () => {
       <div>
         <ChatSearchBar inputValue={value} handler={handler} />
         <ul className='mt-8 flex flex-col gap-5'>
-          {filterdChatRoomList.map((r) => (
+          {filterData.map((r) => (
             <ChatRoomItem key={r.roomId} id={r.roomId} data={r} />
           ))}
         </ul>
