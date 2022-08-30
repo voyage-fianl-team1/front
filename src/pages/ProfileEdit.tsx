fe47d5466b6840eb98fa8e78fbefa4c3016fa266
livemehere <rhdxoals@gmail.com>
2022-08-30 23:25:12 +0900

    Feature : 닉네임 변경 모달 완료

--- src/pages/ProfileEdit.tsx
+++ src/pages/ProfileEdit.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import { Helmet } from 'react-helmet';
+import UserNicknameChange from '../components/UserNIcknameChange';
 
 interface UpdateUser {
   nickname: string;
@@ -12,8 +13,12 @@ interface UpdateUser {
 const ProfileEdit = () => {
   const { nickname } = useSelector((state: RootState) => state.user);
   const [modalShow, setModalShow] = useState(false);
+  const [selected, setSelected] = useState('');
 
-  const toggleModal = useCallback(() => {
+  const toggleModal = useCallback((target?: string) => {
+    if (target) {
+      setSelected(target);
+    }
     setModalShow((prev) => !prev);
   }, []);
 
@@ -29,29 +34,39 @@ const ProfileEdit = () => {
             <h2 className='text-[#38393C]'>{nickname}</h2>
           </div>
           <div>
-            <button className='text-[#38393C] text-[12px] border-[1px] rounded py-1 px-2' onClick={toggleModal}>
+            <button
+              className='text-[#38393C] text-[12px] border-[1px] rounded py-1 px-2'
+              onClick={() => toggleModal('nickname')}
+            >
               수정
             </button>
           </div>
         </div>
         <div className='flex justify-between items-center mt-8'>
           <div>
-            료<h1 className='text-xs'>비밀번호</h1>
+            <h1 className='text-xs'>비밀번호</h1>
             <h2 className='text-[#38393C]'>새로운 비밀번호로 변경 가능</h2>
           </div>
           <div>
-            <button className='text-[#38393C] text-[12px] border-[1px] rounded py-1 px-2' onClick={toggleModal}>
+            <button
+              className='text-[#38393C] text-[12px] border-[1px] rounded py-1 px-2'
+              onClick={() => toggleModal('password')}
+            >
               수정
             </button>
           </div>
         </div>
       </div>
       <div
-        className={`fixed left-0 right-0 bg-blue-50 h-[300px] transition-all ${
+        className={`fixed left-0 right-0 bg-white rounded-t-2xl h-[300px] transition-all ${
           modalShow ? 'bottom-0' : 'bottom-[-300px]'
         }`}
       >
-        modal
+        {selected === 'nickname' ? (
+          <UserNicknameChange toggleModal={toggleModal} />
+        ) : (
+          <div className='text-center'>준비중입니다</div>
+        )}
       </div>
     </>
   );
