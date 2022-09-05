49d7a80a92a1fffd37ba6393a2d86826328de7b3
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-09-05 11:53:47 +0900

    검색 기능 수정

--- src/pages/Searching.tsx
+++ src/pages/Searching.tsx
@@ -1,4 +1,5 @@
-import React, { useCallback, useState, useRef } from 'react';
+import React, { useRef } from 'react';
+import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { keywordAction } from '../redux/features/keywordSlice';
@@ -6,8 +7,8 @@ import { keywordAction } from '../redux/features/keywordSlice';
 const SearchMiddle = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { register, getValues } = useForm({});
   const searchingRef = useRef(null);
-  const [isFocus, setIsFocus] = useState(false);
 
   const handleSearchingOut = (e: React.MouseEvent) => {
     if (e.target === searchingRef.current) {
@@ -15,38 +16,37 @@ const SearchMiddle = () => {
     }
   };
 
-  const handleFocus = useCallback(() => {
-    setIsFocus(true);
-  }, []);
-
-  const handleFocusOut = useCallback(() => {
-    setIsFocus(false);
-  }, []);
-
   const handleKeyword = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      const keyword = e.target.value;
-      dispatch(keywordAction({ keyword: keyword }));
+      dispatch(keywordAction({ keyword: getValues().keyword }));
       navigate(`/keword`);
     }
   };
+
+  const handleButton = () => {
+    dispatch(keywordAction({ keyword: getValues().keyword }));
+    navigate(`/keword`);
+  };
+
   return (
     <>
       <div
-        className={`flex border-[1px] bg-[#FCFCFC] justify-center border-matchgi-gray rounded-full h-[44px] items-center p-[5px]${
-          isFocus ? 'border-[#797CD4]' : ''
-        }`}
+        className={`flex border-[1px] bg-[#FCFCFC] border-matchgi-gray rounded-full h-[44px] items-center 
+        `}
       >
-        <input
-          type='text'
-          placeholder=' 제목, 내용으로 검색'
-          className='flex-1 outline-0 bg-[#FCFCFC] ml-4'
-          onFocus={handleFocus}
-          onBlur={handleFocusOut}
-          onKeyPress={handleKeyword}
-          autoFocus
-        />
-        <img src='/assets/images/search.svg' alt='search-icon' className='w-[24px] h-[24px] mx-[10px]' />
+        <div className='w-full flex flex-row justify-between'>
+          <input
+            type='text'
+            placeholder=' 제목, 내용으로 검색'
+            className='flex-1 outline-0 bg-[#FCFCFC] ml-4'
+            onKeyPress={handleKeyword}
+            autoFocus
+            {...register('keyword')}
+          />
+          <button onClick={handleButton}>
+            <img src='/assets/images/search.svg' alt='search-icon' className='w-[24px] h-[24px] mx-[10px]' />
+          </button>
+        </div>
       </div>
       <div className='w-[100%] h-[90vh] bg-[#FCFCFC]' ref={searchingRef} onClick={handleSearchingOut}></div>
     </>
