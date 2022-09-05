bbd809c451f11e7bedda03fcb967f7de9d439b82
livemehere <rhdxoals@gmail.com>
2022-09-05 18:03:15 +0900

    feature : 홈페이지 배너 슬라이더

--- src/components/SlideBanner.tsx
+++ src/components/SlideBanner.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import { Autoplay } from 'swiper';
+
+const GOOGLE_FORM_LINK = 'https://forms.gle/beTEFQw1Qottddon6';
 
 const SlideBanner = () => {
   return (
     <div className='mt-[20px] mb-[32px]'>
-      <img src='/assets/images/banner/banner1.svg' alt='banner-01' className='w-[100%] h-[100%]' />
+      <Swiper spaceBetween={50} slidesPerView={1} modules={[Autoplay]} autoplay={{ delay: 3000 }}>
+        <SwiperSlide>
+          <img src='/assets/images/banner/banner1.svg' alt='banner-01' className='w-[100%] h-[100%]' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src='/assets/images/banner/banner3.svg' alt='banner-01' className='w-[100%] h-[100%]' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href={GOOGLE_FORM_LINK} className='w-[100%] h-[100%] block' target='_blank' rel='noreferrer'>
+            <img src='/assets/images/banner/banner2.svg' alt='banner-01' className='w-[100%] h-[100%]' />
+          </a>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
