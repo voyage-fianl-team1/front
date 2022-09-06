bab82ae8b0393f07bb2a739795f732b2d798bf21
livemehere <rhdxoals@gmail.com>
2022-09-06 20:23:42 +0900

    fix : 홈화면 최신 경기목록 content 추가

--- src/components/MatchItem.tsx
+++ src/components/MatchItem.tsx
@@ -7,7 +7,7 @@ import { convertDateShort } from '../util/converDate';
 
 const MatchItem = () => {
   const navigate = useNavigate();
-  const res = useQuery(['recentMatchList'], async () => await apis.getMatchItem());
+  const res = useQuery(['recentMatchList'], apis.getMatchItem);
 
   const recentList = res?.data?.data.content;
 
@@ -16,13 +16,14 @@ const MatchItem = () => {
       {recentList &&
         recentList.map((v: RecentMatch, i: number) => (
           <div className='recent-match-card' key={i}>
-            <h1 className='font-semibold text-[14px] mb-1 truncate ...'>{v.title}</h1>
+            <h1 className='font-semibold text-[14px] mb-[7px] truncate ...'>{v.title}</h1>
             <h2 className='text-[14px]'>{v.subject}</h2>
-            <p className='mt-[38px] text-[12px] text-matchgi-gray truncate ...'>{v.address}</p>
+            <h2 className='text-[14px]'>{v.content}</h2>
+            <p className='text-[12px] mt-[24px] text-matchgi-gray truncate ...'>{v.address}</p>
             <div className='flex justify-between items-center mt-[12px]'>
               <span className='text-sm text-matchgi-gray'>{convertDateShort(v.createdAt)}</span>
               <button
-                className='border-[1px] border-[#C5C6CA] text-sm px-[8px] py-[3px] rounded text-[#5D5E62] text-[12px] '
+                className='border-[1px] border-[#C5C6CA] text-sm px-[8px] pt-[2px] rounded text-[#5D5E62] text-[12px] '
                 onClick={() => navigate(`/match/${v.postId}`)}
               >
                 더보기
